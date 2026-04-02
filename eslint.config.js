41aff0e5eff9608b62dd123a77016becd1fa27c3
John Dave Dalmao <johndavedalmao@gmail.com>
2026-04-02 15:43:32 +0800

    eslint and prettier format config setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,56 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import { defineConfig, globalIgnores } from 'eslint/config';
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import prettierPlugin from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
-export default defineConfig([
-   globalIgnores(['dist']),
-   {
-      files: ['**/*.{ts,tsx}'],
-      extends: [
-         js.configs.recommended,
-         tseslint.configs.recommended,
-         reactHooks.configs.flat.recommended,
-         reactRefresh.configs.vite,
-      ],
-      languageOptions: {
-         ecmaVersion: 2020,
-         globals: globals.browser,
+export default [
+  {
+    ignores: ["dist", "node_modules"],
+  },
+
+  js.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  react.configs.flat.recommended,
+  reactHooks.configs.flat.recommended,
+  reactRefresh.configs.vite,
+  jsxA11y.flatConfigs.recommended,
+
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
       },
-   },
-]);
+    },
+    plugins: {
+      prettier: prettierPlugin,
+      "simple-import-sort": simpleImportSort,
+      "unused-imports": unusedImports,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "prettier/prettier": "error",
+      "unused-imports/no-unused-imports": "error",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+
+  prettier,
+];
