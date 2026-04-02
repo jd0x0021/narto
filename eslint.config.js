ed8c01db33584e272720d92ec4df2517bddcb425
John Dave Dalmao <johndavedalmao@gmail.com>
2026-04-02 17:04:49 +0800

    disable explicit import of React rule

--- eslint.config.js
+++ eslint.config.js
@@ -43,6 +43,7 @@ export default [
 			},
 		},
 		rules: {
+			'react/react-in-jsx-scope': 'off',
 			'react-hooks/rules-of-hooks': 'error',
 			'react-hooks/exhaustive-deps': 'warn',
 			'prettier/prettier': 'error',
