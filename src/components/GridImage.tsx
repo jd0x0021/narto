938ab42210efb4488f0d43921868e9f529007521
jd0x0021 <johndavedalmao@gmail.com>
2026-08-16 17:04:18 +0800

    update the copy messages for png or gif, and reduced the copy indicator text

--- src/components/GridImage.tsx
+++ src/components/GridImage.tsx
@@ -160,11 +160,11 @@ function GridImageComponent({
 					{copying ? (
 						<div className='size-8 border-4 border-gray-300 border-t-narto-accent/80 rounded-full animate-spin'></div>
 					) : isCopied ? (
-						<span className='bg-green-500 text-narto-text rounded-md px-2 py-1 text-sm mx-2 text-center max-w-[90%] shadow-sm'>
-							Copied 😼
+						<span className='bg-green-500 text-narto-text rounded-md px-2 py-1 text-xs mx-2 text-center max-w-[90%] shadow-sm'>
+							Copied {image.format === 'gif' ? 'GIF URL' : 'PNG File'} 😼
 						</span>
 					) : copyErrored ? (
-						<span className='bg-red-600 text-narto-text rounded-md px-2 py-1 text-sm mx-2 text-center max-w-[90%] shadow-sm'>
+						<span className='bg-red-600 text-narto-text rounded-md px-2 py-1 text-xs mx-2 text-center max-w-[90%] shadow-sm'>
 							Copy failed 💀
 						</span>
 					) : null}
